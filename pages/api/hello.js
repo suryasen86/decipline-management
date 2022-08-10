63283e57fc5b814d1a7a8770630ce5f64ff7c216
tonudon86 <tonudon86@gmail.com>
2022-08-10 10:04:39 +0530

    inital commit

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  console.log(req.body)
+  
+  let name=req.body.name
+  if(name=="suryasen"){
+    res.status(200).json({ status:true,name: 'John Doe' })
+  }else{
+    res.status(200).json({ status:false,name: 'John Doe' })
+  }
+ 
 }
---
+++ pages/home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const home = () => {
+  return (
+    <div>hello suryasen</div>
+  )
+}
+
+export default home
