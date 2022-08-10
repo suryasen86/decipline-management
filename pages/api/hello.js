1426197e11198119941f777ca77140e8bb28d430
tonudon86 <tonudon86@gmail.com>
2022-08-10 10:06:06 +0530

    inital commit

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -2,12 +2,6 @@
 
 export default function handler(req, res) {
   console.log(req.body)
-  
-  let name=req.body.name
-  if(name=="suryasen"){
-    res.status(200).json({ status:true,name: 'John Doe' })
-  }else{
-    res.status(200).json({ status:false,name: 'John Doe' })
-  }
- 
+
+
 }
